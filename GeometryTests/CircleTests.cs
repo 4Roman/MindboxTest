fea1c47b1e7123937b01d61c80017e1e72bad909
4Roman <63200666+4Roman@users.noreply.github.com>
2022-05-21 03:44:04 +0300

    UnitTest4CIrcle

--- GeometryTests/CircleTests.cs
+++ GeometryTests/CircleTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Geometry.Circle;
+using System;
 
 namespace GeometryTests
 {
@@ -7,16 +8,57 @@ namespace GeometryTests
     public class CircleTests
     {
         [Test]
-        public void CalcSquare_StateUnderTest_ExpectedBehavior()
+        public void CalcSquare_Circle_0_ShouldReturn_0()
         {
             // Arrange
-            var circle = Circle.CreateInstance(0.0);
+            var circle = Circle.CreateInstance(0);
 
             // Act
             var result = circle.CalcSquare();
 
             // Assert
-            Assert.Fail();
+            Assert.AreEqual(0.0, result, 0.001);
         }
+
+        [Test]
+        public void CalcSquare_Circle_1_ShouldReturn_Pi()
+        {
+            // Arrange
+            var circle = Circle.CreateInstance(1);
+
+            // Act
+            var result = circle.CalcSquare();
+
+            // Assert
+            Assert.AreEqual(Math.PI, result, 0.001);
+        }
+
+        [Test]
+        public void CalcSquare_Circle_3_ShouldReturn_9Pi()
+        {
+            // Arrange
+            var circle = Circle.CreateInstance(3);
+
+            // Act
+            var result = circle.CalcSquare();
+
+            // Assert
+            Assert.AreEqual(9*Math.PI, result, 0.001);
+        }
+
+        [Test]
+        public void CalcSquare_Circle_minus_1_ShouldReturn_IllegalCircleRadiusMustNotBeLessThanZeroException()
+        {
+            // Assert
+            Assert.Throws<Geometry.Circle.IllegalCircleRadiusMustNotBeLessThanZeroException>(() =>
+            {
+                var triangle = Circle.CreateInstance(-1);
+                var result = triangle.CalcSquare();
+            });
+        }
+
+
+
+
     }
 }
---
+++ MindboxTest/Circle/IllegalCircleRadiusMustNotBeLessThanZeroException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry.Circle
+{
+    public class IllegalCircleRadiusMustNotBeLessThanZeroException : Exception
+    {
+        public IllegalCircleRadiusMustNotBeLessThanZeroException(string message, Exception? innerException, double radius) : base(message, innerException)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; }
+    
+    }
+}
